75cfb87246a2af64cda0b18bb34f1f1352d938cb
eaj0009 <89614100+eaj0009@users.noreply.github.com>
2021-12-17 19:18:08 -0600

    framer motion nav

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { motion, Variants } from 'framer-motion';
+import { useState } from 'react';
 
 interface PageLink {
   route: string;
@@ -21,6 +23,52 @@ export const navbarLinks = [
 ];
 
 const Navbar = () => {
+  const navVariants: Variants = {
+    open: {
+      x: '100%',
+      transition: {
+        ease: 'linear',
+        duration: 0.15,
+      },
+    },
+    close: {
+      x: '0%',
+      transition: {
+        ease: 'linear',
+        duration: 0.15,
+      },
+    },
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const Hamburger = () => (
+    <button
+      className='navbar__hamburger'
+      aria-label='open menu button'
+      onClick={() => setIsOpen(true)}>
+      <svg
+        className='navbar__hamburgericon'
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'>
+        <path d='M0 0h24v24H0V0z' fill='none' />
+        <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
+      </svg>
+    </button>
+  );
+
+  const ExitIcon = () => (
+    <button
+      className='navbar__exit'
+      aria-label='exit menu button'
+      onClick={() => setIsOpen(false)}>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M0 0h24v24H0V0z' fill='none' />
+        <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z' />
+      </svg>
+    </button>
+  );
+
   return (
     <>
       <header>
@@ -29,15 +77,17 @@ const Navbar = () => {
             <Link href='/'>
               <a className='navbar__logo'>Logo</a>
             </Link>
-            <div id='hamburger-menu' className='navbar__links'>
-              {/* {navbarLinks.map((link) => (
+            <motion.div
+              initial='close'
+              variants={navVariants}
+              animate={isOpen ? 'open' : 'close'}
+              id='hamburger-menu'
+              className='navbar__links'>
+              {navbarLinks.map((link) => (
                 <PageLink key={link.name} route={link.route} name={link.name} />
-              ))} */}
-              <PageLink route='/projects' name='Projects' />
-              <PageLink route='/#about' name='About' />
-              <PageLink route='/contact' name='Contact' />
+              ))}
               <ExitIcon />
-            </div>
+            </motion.div>
             <Hamburger />
           </div>
         </nav>
@@ -48,21 +98,19 @@ const Navbar = () => {
 };
 
 const closeMenu = () => {
-  const links: HTMLElement | null = document.getElementById('hamburger-menu');
-
-  if (links?.classList.contains('open-nav') == true) {
-    links?.classList.remove('open-nav');
-  }
+  // const links: HTMLElement | null = document.getElementById('hamburger-menu');
+  // if (links?.classList.contains('open-nav') == true) {
+  //   links?.classList.remove('open-nav');
+  // }
 };
 
 const changeMenu = () => {
-  const links: HTMLElement | null = document.getElementById('hamburger-menu');
-
-  if (links?.classList.contains('open-nav') == true) {
-    links?.classList.remove('open-nav');
-  } else {
-    links?.classList.add('open-nav');
-  }
+  // const links: HTMLElement | null = document.getElementById('hamburger-menu');
+  // if (links?.classList.contains('open-nav') == true) {
+  //   links?.classList.remove('open-nav');
+  // } else {
+  //   links?.classList.add('open-nav');
+  // }
 };
 
 const PageLink = ({ route, name }: PageLink) => (
@@ -71,30 +119,6 @@ const PageLink = ({ route, name }: PageLink) => (
       {name}
     </a>
   </Link>
-);
-
-const Hamburger = () => (
-  <button
-    className='navbar__hamburger'
-    aria-label='open menu button'
-    onClick={() => changeMenu()}>
-    <svg
-      className='navbar__hamburgericon'
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 24 24'>
-      <path d='M0 0h24v24H0V0z' fill='none' />
-      <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
-    </svg>
-  </button>
-);
-
-const ExitIcon = () => (
-  <button className='navbar__exit' aria-label='exit menu button' onClick={() => changeMenu()}>
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-      <path d='M0 0h24v24H0V0z' fill='none' />
-      <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z' />
-    </svg>
-  </button>
 );
 
 export default Navbar;
