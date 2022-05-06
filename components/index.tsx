5894b539e501916a064d390138625a6bab601715
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-06 14:42:57 -0500

    added components

--- components/index.tsx
+++ components/index.tsx
@@ -3,9 +3,13 @@ export { default as Navbar } from '@/components/shared/Navbar';
 export { default as Section } from '@/components/shared/Section';
 export { default as Footer } from '@/components/shared/Footer';
 export { default as HeadOpenGraph } from '@/components/shared/HeadOpenGraph';
+export { default as BlurImage } from '@/components/shared/BlurImage';
 
 export { default as CTA } from '@/components/home/CTA';
 export { default as AboutMe } from '@/components/home/AboutMe';
+
+export { default as FeedLinks } from '@/components/projects/FeedLinks';
+export { default as RichTextRender } from '@/components/projects/RichTextRender';
 
 export { CustomInput as CustomInput } from '@/components/contact/FormInputs';
 export { TextFieldInput as TextFieldInput } from '@/components/contact/FormInputs';
