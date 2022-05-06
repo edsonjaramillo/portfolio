b3186ddd90973dfa97d3a955f87fe1c6a63ad46d
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-06 14:51:31 -0500

    added feed project

---
+++ components/projects/FeedProject.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import type { CategoryType, ProjectType } from '@/lib/graphcms';
+import { BlurImage } from '@/components/index';
+import FeedLinks from './FeedLinks';
+
+export const FeedProjectItem = (project: ProjectType) => (
+  <article className='feedproject'>
+    <Link href={`/project/${project.slug}`}>
+      <a className='feedproject__image'>
+        <BlurImage
+          src={project.image.url}
+          title={project.image.alt}
+          alt={project.image.alt}
+          layout='fill'
+          objectFit='fill'
+        />
+      </a>
+    </Link>
+    <div className='feedproject__details'>
+      <Link href={`/project/${project.slug}`}>
+        <a title={project.name} className='feedproject__title'>
+          {project.name}
+        </a>
+      </Link>
+      <span>{project.description}</span>
+      <FeedLinks {...project.projectlink} />
+    </div>
+  </article>
+);
