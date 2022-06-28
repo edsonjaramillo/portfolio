464f54cb7c4e1a8a81103b227131c9ac062c3e66
edsonjaramillo <edsonajaramillo@gmail.com>
2022-06-28 14:55:00 -0500

    added font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,14 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-
   render() {
     return (
       <Html lang='en'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap'
             rel='stylesheet'
           />
         </Head>
